1e4f1d02078c265bc41220bc0fbb79392dedf8fb
Orkhan Karimov <naxro.ok1@gmail.com>
2025-03-22 03:40:36 +0400

    feat: add more methods

--- src/enums/http-method.enum.ts
+++ src/enums/http-method.enum.ts
@@ -2,16 +2,24 @@
  * Enum representing HTTP methods
  */
 export enum HttpMethod {
+	// Standard HTTP methods (RFC 7231)
 	GET = 'get',
 	POST = 'post',
 	PUT = 'put',
 	DELETE = 'delete',
-	PATCH = 'patch',
-	ALL = 'all',
-	OPTIONS = 'options',
 	HEAD = 'head',
+	OPTIONS = 'options',
 	CONNECT = 'connect',
 	TRACE = 'trace',
+
+	// HTTP PATCH (RFC 5789)
+	PATCH = 'patch',
+
+	// PATCH framework for HTTP (RFC 5842)
+	REPORT = 'report',
+	SEARCH = 'search',
+
+	// WebDAV methods (RFC 4918)
 	PROPFIND = 'propfind',
 	PROPPATCH = 'proppatch',
 	MKCOL = 'mkcol',
@@ -19,20 +27,69 @@ export enum HttpMethod {
 	MOVE = 'move',
 	LOCK = 'lock',
 	UNLOCK = 'unlock',
-	PURGE = 'purge',
-	LINK = 'link',
-	UNLINK = 'unlink',
-	MERGE = 'merge',
-	REPORT = 'report',
-	SEARCH = 'search',
+
+	// WebDAV versioning extensions (RFC 3253)
+	VERSION_CONTROL = 'version-control',
+	BASELINE_CONTROL = 'baseline-control',
+	LABEL = 'label',
+	CHECKIN = 'checkin',
+	UNCHECKOUT = 'uncheckout',
+	MKWORKSPACE = 'mkworkspace',
+	UPDATE = 'update',
+
+	// CalDAV (RFC 4791)
+	MKCALENDAR = 'mkcalendar',
+
+	// WebDAV Binding Extensions (RFC 5842)
+	BIND = 'bind',
+	UNBIND = 'unbind',
+	REBIND = 'rebind',
+
+	// Additional WebDAV extensions
+	ACL = 'acl',
+
+	// UPnP specific
+	M_SEARCH = 'm-search',
+
+	// Icecast streaming specific
+	SOURCE = 'source',
+
+	// RTSP methods (RFC 2326)
+	DESCRIBE = 'describe',
+	ANNOUNCE = 'announce',
+	SETUP = 'setup',
+	PLAY = 'play',
+	PAUSE = 'pause',
+	TEARDOWN = 'teardown',
+	GET_PARAMETER = 'get_parameter',
+	SET_PARAMETER = 'set_parameter',
+	REDIRECT = 'redirect',
+	RECORD = 'record',
+
+	// RAOP (Remote Audio Output Protocol)
+	FLUSH = 'flush',
+
+	// Draft HTTP method with request body
+	QUERY = 'query',
+
+	// Miscellaneous and CDN-specific methods
+	PURGE = 'purge', // Used by some CDNs like Fastly
+	LINK = 'link', // Used in various contexts
+	UNLINK = 'unlink', // Used in various contexts
+	MERGE = 'merge', // Used in various contexts
+
+	// Methods used in various implementations
 	MKACTIVITY = 'mkactivity',
 	CHECKOUT = 'checkout',
 	NOTIFY = 'notify',
 	SUBSCRIBE = 'subscribe',
 	UNSUBSCRIBE = 'unsubscribe',
+
+	// HTTP/2 specific (RFC 7540)
+	PRI = 'pri',
 }
 
 /**
  * Type for HTTP methods, allowing both enum values and custom string methods
  */
-export type HttpMethodType = HttpMethod | string;
+export type HttpMethodType = HttpMethod | string
